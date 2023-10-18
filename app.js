d6050b105c35787dc21f7b6cb84e2c12f6e261ac
psykerdev <psykrdev@proton.me>
2023-10-18 13:16:25 -0500

    commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function readJsonData(){
+    console.log("json data read")
+}
+function createJsonData(){
+    console.log("created json data")
+
+}
+function saveJsonData(){
+    console.log("save json data")
+}
