84caad9de82ff85d1794648b35318a90e43e3385
psykerDev <psykerdev@proton.me>
2023-10-23 19:00:42 -0400

    fixing up

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
-function readJsonData(){
-    console.log("json data read")
-}
-function createJsonData(){
-    console.log("created json data")
+// const WorkOutData = {
+//   Index: 0,
+//   TileDate: Date,
+//   Split: "",
+//   Reps: 0,
+//   Sets: 0,
+//   Notes: "",
+// };
+// let savedData = [];
 
+function daysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
-function saveJsonData(){
-    console.log("save json data")
+
+function getDaysInMonth() {
+  let date = new Date();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let days = daysInMonth(month, year);
+  console.log(days);
+  return days;
+}
+
+function populateList() {
+  let mainContainer = document.getElementById("main_container");
+  let listItem = document.getElementById("list_item");
+  let i = 0;
+
+  while (i < getDaysInMonth()) {
+    var clonedLitem = listItem.cloneNode(true);
+
+    clonedLitem.removeAttribute("id");
+
+    mainContainer.appendChild(clonedLitem);
+    i++;
+  }
+  listItem.remove();
+}
+populateList();
+
+function togglePopup() {
+  let DataPopup = document.getElementById("popup");
+  if (DataPopup.style.display === "none") {
+    DataPopup.style.display = "flex";
+  } else {
+    DataPopup.style.display = "none";
+  }
 }
